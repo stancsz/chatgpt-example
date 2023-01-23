a504f80f928062babcb0b91df5c1abea8ffdc3a4
Stan Chen <43565411+stancsz@users.noreply.github.com>
2023-01-22 18:39:37 -0700

    Work in progress to implement a rolling context mechamism

--- main.py
+++ main.py
@@ -8,11 +8,28 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 # Setting up the logging feature by creating a file with the topic name
 topic = "demo"
 history_log = 'history/' + re.sub('[^0-9a-zA-Z]+', '', topic) + '.log'
-file = open(history_log, "a")
 
 # Initializing the prompt and context variables
 prompt = ""
 context = ""
+
+
+# How many last prompt will the script pass to the next prompt
+
+def estimate_tokens(text):
+    """
+    As per https://openai.com/api/pricing/, prices are per 1,000 tokens. You can think of tokens as pieces of words, where 1,000 tokens is about 750 words. This paragraph is 35 tokens.
+    :param text:
+    :return:
+    """
+    return len(text.split()) / 0.75
+
+
+def reduce_context(context, limit=3000):
+    context_list = context.split()
+    new_size = int(limit / 0.75)
+    return " ".join(context.split()[-new_size:])
+
 
 while True:
     # Prints '>>' to indicate user input is needed
@@ -22,24 +39,28 @@ while True:
     # If the user inputs 'exit', the loop breaks
     if prompt == 'exit':
         break
-    # Writes the user's input to the log file
-    file.write(prompt)
+
     # Sends the prompt and context to the OpenAI API
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt="context:" + context + "\n\n" + "prompt:" + prompt,
+        prompt="context:" + context + "\n" + "prompt:" + prompt,
         temperature=0.7,
         max_tokens=4000,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0
     )
+    # Writes the user's input to the log file
+    file = open(history_log, "a")
+    file.write(prompt)
     # Writes the API's response to the log file
     file.write(response["choices"][0]["text"] + "\n")
     # Prints the API's response
     print(response["choices"][0]["text"] + "\n")
     # Adds the prompt and response to the context variable
-    context += "\n".join([context, prompt, response["choices"][0]["text"]])
-
-# Closes the log file
-file.close()
+    context += prompt + "/n"
+    print(f"context[{context}]")
+    print(estimate_tokens(context))
+    print(reduce_context(context))
+    # Closes the log file
+    file.close()
